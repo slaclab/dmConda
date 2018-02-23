f45dacbc10eb9e174182bc2d11fed31fcf00ffde
Murali Shankar <mshankar@slac.stanford.edu>
2018-02-23 15:11:27 -0800

    Check in condaBuildFromGit.sh from previous repo

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # dmConda
-Scripts for creating and installing conda packages for the PCDS DM enviroment
+This repo contains scripts and information for building packages, creating and managing conda environments for PCDS Data Management.
+To use the PCDS DM conda, 
+```
+source /reg/g/psdm/sw/dmconda/etc/profile.d/conda.sh
+```
+and then use `conda activate <env>` to activate the named environment.
+A list of conda envs can be obtained using `conda info --envs`.
+
+
+### Building conda packages directly from GitHub.
+For home-grown packages that have conda recipes, use `condaBuildFromGit.sh` to
+build and register the build artifact with the DM custom channel at `/reg/g/psdm/sw/dmconda/channels/rhel7`.
+For example, 
+```
+./condaBuildFromGit.sh https://github.com/slaclab/flask_socket_util.git
+```
+builds and registers `flask_socket_util`. The version used for the built artifact is determined by the version/tag in the `meta.yaml`.
+
+To later install the built artifact into your environment, `conda activate` your enviromnent and use `conda install` like so
+```
+conda install -c /reg/g/psdm/sw/dmconda/channels/rhel7 flask_socket_util
+```
+ 
+
+
+
+
+
+
---
+++ condaBuildFromGit.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# A small script to build conda packages directly from the git repo.
+# Clones the repo and call conda build on the recipe within the repo
+# The build artifact if any is automatically placed in the DM channel
+# located here - /reg/g/psdm/sw/dmconda/channels/rhel7
+# To later install a package from here - use something like so 
+# conda install -c /reg/g/psdm/sw/dmconda/channels/rhel7 <packagename>
+
+
+die() {
+  echo "$1" 2>&1
+  exit 1
+}
+
+[ "$#" -eq 1 ] || die "Please specify the location of the git repo for the package"
+
+export REPO_URL=$1
+
+source /reg/g/psdm/sw/dmconda/etc/profile.d/conda.sh
+
+export DM_CHANNEL_PATH=/reg/g/psdm/sw/dmconda/channels/rhel7
+
+export PACKAGENAME=`basename ${REPO_URL} .git`
+
+cd /tmp
+export TEMPFOLDER=`mktemp -d condaBuild_XXX`
+
+pushd ${TEMPFOLDER}
+
+function cleanup {
+  popd
+  echo "Cleaning up folder `pwd`/${TEMPFOLDER}"
+  rm -rf "${TEMPFOLDER}"
+}
+
+trap cleanup EXIT
+
+echo "Building package ${PACKAGENAME} in `pwd`"
+
+git clone --recursive ${REPO_URL}
+
+export FULL_PATH_TO_RECIPE=`pwd`/${PACKAGENAME}/recipe
+
+[ -d "${FULL_PATH_TO_RECIPE}" ] || die "Missing recipe in package ${FULL_PATH_TO_RECIPE}"
+
+conda build \
+    --py 3.6 \
+    --no-anaconda-upload \
+    --skip-existing \
+    --no-locking \
+    --channel conda-forge \
+    --croot ${DM_CHANNEL_PATH} \
+    ${FULL_PATH_TO_RECIPE}
+
+
+cd ..
+
+echo "Done building"
+
